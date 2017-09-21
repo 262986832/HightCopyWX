4330861d15bebddd5a2407a20e878b85e22d7cad
fvelement <262986832@qq.com>
2017-09-21 11:16:14 +0800

    完善学习模块

--- app/src/main/java/com/idisfkj/hightcopywx/chat/model/imp/ChatModelStudyImp.java
+++ app/src/main/java/com/idisfkj/hightcopywx/chat/model/imp/ChatModelStudyImp.java
@@ -54,7 +54,6 @@ public class ChatModelStudyImp extends ChatModelBase implements ChatModelStudy {
                             insert(respondStudy.getListWords());
                             listener.onInitSucceed();
                         }
-
                     }
                 }, new Response.ErrorListener() {
             @Override
